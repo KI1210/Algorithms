00141943060f306c2dd5901bd98114a44e2ae3c3
Kasun Isuranga <158312737+KI1210@users.noreply.github.com>
2024-02-15 16:07:34 +0530

    Merge Sort

---
+++ Sorting/Merge Sort/merge.py
@@ -0,0 +1,40 @@
+from ..randomArray import getRandomArray
+
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    
+    leftArr = arr[:mid]
+    rightArr = arr[mid:]
+
+    leftArr = mergeSort(leftArr)
+    rightArr = mergeSort(rightArr)
+
+    return merge(leftArr, rightArr)
+
+def merge(leftArr, rightArr):
+    arr = []
+    leftIdx, rightIdx = 0, 0
+    
+    while leftIdx < len(leftArr) and rightIdx < len(rightArr):
+        if leftArr[leftIdx] < rightArr[rightIdx]:
+            arr.append(leftArr[leftIdx])
+            leftIdx += 1
+        else:
+            arr.append(rightArr[rightIdx])
+            rightIdx += 1
+    
+    arr.extend(leftArr[leftIdx:])
+    arr.extend(rightArr[rightIdx:])
+    
+    return arr
+
+arr = getRandomArray(10)
+
+print(arr)
+
+arr = mergeSort(arr)
+
+print(arr)

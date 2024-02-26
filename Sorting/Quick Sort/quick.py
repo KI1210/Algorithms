4d2ae07b64d53abc3cb8d105eb4403d73b2720ca
Kasun Isuranga <158312737+KI1210@users.noreply.github.com>
2024-02-26 17:31:16 +0530

    Implemented Quick Sort

---
+++ Sorting/Quick Sort/quick.py
@@ -0,0 +1,21 @@
+from ..randomArray import getRandomArray
+
+
+def quickSort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+
+    lesser = [x for x in arr if x < pivot]
+    equal = [x for x in arr if x == pivot]
+    greater = [x for x in arr if x > pivot]
+
+    return quickSort(lesser) + equal + quickSort(greater)
+
+
+arr = getRandomArray(10)
+print(arr)
+
+arr = quickSort(arr)
+print(arr)
